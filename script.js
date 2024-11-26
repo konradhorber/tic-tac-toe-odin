13629a101117ed0b56dbe75f9e9f3bb95ece3f6e
konradhorber <konrad.horber@gmail.com>
2024-11-26 12:22:05 +0000

    Add tic tac toe game logic

--- script.js
+++ script.js
@@ -13,17 +13,69 @@ function createGameboard() {
         for (let currentRow = 0; currentRow < gameboard.length; currentRow++) {
             for (let currentCol = 0; currentCol  < gameboard[currentRow].length; currentCol++) {
                 if (currentRow == updateRow && currentCol == updateCol) {
-                    if(gameboard[currentRow] == ' ' && gameboard[currentCol] == ' ') {
+                    if(gameboard[currentRow][currentCol] == ' ') {
                         gameboard[currentRow][currentCol] = player;
-                        return;
+                        return false;
                     }
-                    return;
+                    return true;
                 }
             }
         }
     }
 
-    return {gameboard, print, update};
+    function checkWon() {
+        for (let i=0; i < gameboard.length; i++) {
+            let firstPlayer = gameboard[i][0];
+            if (firstPlayer == ' ') {
+                continue;
+            }
+            for (let j=0; j < gameboard.length; j++) {
+                if (gameboard[i][j] != firstPlayer){
+                    break;
+                }else if (j==2) {
+                    return firstPlayer;
+                }
+            }
+        }
+        for (i=0; i < gameboard.length; i++) {
+            let firstPlayer = gameboard[0][i];
+            if (firstPlayer == ' ') {
+                continue;
+            }
+            for (let j=0; j < gameboard.length; j++) {
+                if (gameboard[j][i] != firstPlayer) {
+                    break;
+                }else if (j == 2) {
+                    return firstPlayer;
+                }
+            }
+        }
+        let firstPlayer = gameboard[0][0]
+        for (i=0; i < gameboard.length; i++) {
+            if (firstPlayer == ' ') {
+                break;
+            }
+            if(gameboard[i][i] != firstPlayer) {
+                break;
+            }else if (i == 2) {
+                return firstPlayer;
+            }
+        }
+        firstPlayer = gameboard[0][gameboard.length-1]
+        for (i=0; i < gameboard.length; i++) {
+            if (firstPlayer == ' ') {
+                break;
+            }
+            if(gameboard[i][gameboard.length-1-i] != firstPlayer) {
+                break;
+            } else if (i == 2) {
+                return firstPlayer;
+            }
+        }
+        return false;
+    }
+
+    return {gameboard, print, update, checkWon};
 }
 
 function createPlayers() {
@@ -39,12 +91,23 @@ function createGame() {
         for (player of players) {
             gameboard.print();
             console.log(`Player ${player}:`)
-            let inputRow = prompt(`${player} enter your input as row: `);
-            let inputCol = prompt('and col: ');
-            gameboard.update(player, inputRow, inputCol);
+            let inputRow = null;
+            let inputCol = null;
+            do {
+                inputRow = prompt(`${player} enter your input as row: `);
+                inputCol = prompt('and col: ');
+            }while(gameboard.update(player, inputRow, inputCol));
+            if(gameboard.checkWon() != false) {
+                gameboard.print();
+                return;
+            }
         }
     }
-    return {playRound};
+    return {playRound, gameboard};
 }
 
 const game = createGame();
+do{
+    game.playRound();
+} while (game.gameboard.checkWon() == false);
+console.log(`Player ${game.gameboard.checkWon()} has won!`);
